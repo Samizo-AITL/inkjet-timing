bb65df3331a5f422e8eb8ea3ec7a3e58db96f75c
Shinichi Samizo <shin3t72@gmail.com>
2025-12-29 15:00:37 +0900

    Update params.js

--- docs/params.js
+++ docs/params.js
@@ -1,6 +1,6 @@
-export const TIME_VIEW = 40e-6;   // 40 µs
 export const DT = 0.1e-6;
-export const N = Math.floor(TIME_VIEW / DT);
+export const T_END = 40e-6;
+export const N = Math.floor(T_END / DT);
 
 export let gains = { V:1, I:1, x:1, P:1, Q:1 };
 
