7e3d8e9d3ced1ba76f8ad57f141e2c91ad95b2cc
Shinichi Samizo <shin3t72@gmail.com>
2025-12-29 13:12:56 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -2,7 +2,20 @@ import { params } from "./params.js";
 import { computeStack } from "./model.js";
 import { drawStack } from "./draw.js";
 
-console.log("BOOT OK");
+console.log("MAIN BOOT OK");
 
+const canvas = document.getElementById("scope");
+const ctx = canvas.getContext("2d");
+
+// 仮テスト（まず塗る）
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = "lime";
+ctx.beginPath();
+ctx.moveTo(0, canvas.height / 2);
+ctx.lineTo(canvas.width, canvas.height / 2);
+ctx.stroke();
+
+// 本来の処理
 const data = computeStack(params);
-drawStack(data);
+drawStack(ctx, data);
